37fa934cf77e8399f94935215f76e71eb682f509
bryanm-lagash <bryanm@lagash.com>
2020-02-17 14:48:15 -0300

    Un camibio x

--- conexionBD/src/index.js
+++ conexionBD/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 
@@ -13,46 +13,43 @@ const connection = mysql.createConnection({
     database: "react_sort"
 });
 
-connection.connect(err =>{
-    if(err){
-        return err;
-    }
-})
-
-
-app.use(cors())
-
-app.get('/', (req, res) => {
-res.send('buena boludoo')
-})
-
-app.get('/users', (req, res) => {
-    connection.query(SELECT_ALL_USERS_QUERY, (err,results) =>{
-        if (err) {
-         return res.send(err);   
-        }else{
-            res.json({
-                data: results
-            })
-        }
-
-    } )
+connection.connect(err => {
+  if (err) {
+    return err;
+  }
 });
-app.get('/users/add', (req, res) => {
-   //estructura de tabla
-    const {nombre} = req.query;
-    console.log(nombre);
-    const INSERT_USER_QUERY = `INSERT INTO user (nombre) VALUES('${nombre}')`
-    connection.query(INSERT_USER_QUERY, (err, result) =>{
-        if(err){
-           return  res.send(err);
-        }else{
-            return res.send('añadido correctamente')
-        }
-    })
-    })
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("buena boludoo");
+});
+
+app.get("/users", (req, res) => {
+  connection.query(SELECT_ALL_USERS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      res.json({
+        data: results
+      });
+    }
+  });
+});
+app.get("/users/add", (req, res) => {
+  //estructura de tabla
+  const { nombre } = req.query;
+  console.log(nombre);
+  const INSERT_USER_QUERY = `INSERT INTO user (nombre, admin) VALUES('${nombre}', false)`;
+  connection.query(INSERT_USER_QUERY, (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send("añadido correctamente");
+    }
+  });
+});
 
 app.listen(4000, () => {
-    console.log('server running on port 4000')
-})
-
+  console.log("server running on port 4000");
+});
